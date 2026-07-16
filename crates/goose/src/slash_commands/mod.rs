ded364bf00cbd8dc3dbe6f83201f20fbcbdbd56a
hahaha <decailin007@gmail.com>
2026-07-16 16:26:03 +0800

    chore: recover enterprise implementation bundle

--- crates/goose/src/slash_commands/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod recipe_slash_command;
-pub mod skill_slash_command;
-pub mod slash_command;
-pub mod types;
-pub mod util;
--- products/meteo-office-desktop/services/skillhub/go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/dc365/goose/products/meteo-office-desktop/services/skillhub
-
-go 1.22
